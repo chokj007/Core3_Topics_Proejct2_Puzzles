f68ab7d4506be7ad8dd738d157fdc9f0abdf2bc1
chokj007 <98773970+chokj007@users.noreply.github.com>
2023-10-22 23:27:26 -0400

    changes

--- Game/page4.js
+++ Game/page4.js
@@ -1,5 +1,4 @@
-
-// shuffle array randomly
+//shuffle array randomly
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -54,27 +53,27 @@ document.addEventListener('DOMContentLoaded', function () {
         'RGB(145, 56, 99)',
         'RGB(159, 61, 115)',
         'RGB(173, 66, 130)',
-    ]; // Array of colors
+    ]; 
 
-    //  shuffle and randomize the initial background colors of the divs
-    function randomizeInitialColors() {
+     // shuffle and randomize the initial background colors of the divs
+     function randomizeInitialColors() {
         const mainDivs = document.querySelectorAll('.grid-cell');
         shuffledColors = [...colors];
         shuffleArray(shuffledColors);
-    
+
         mainDivs.forEach(function (div, index) {
-            if (index === 0 || index === 2 || index === 4 || index === 7 || index === 12 ||index === 15 || index === 16 || index === 17 || index === 18 || index === 19 || index === 22 || index === 27 || index === 30 || index === 32 || index === 34) {
+            if (index === 0 || index === 2 || index === 4 || index === 7 || index === 12 || index === 15 || index === 16 || index === 17 || index === 18 || index === 19 || index === 22 || index === 27 || index === 30 || index === 32 || index === 34) {
                 // If it's specified cells, set its color to the preset color and add a black dot
                 setBackgroundColor(div, predeterminedColors[div.id]);
                 addBlackDotToSpecificDivs(div);
             } else {
-                // Otherwise, set a shuffled color
+                // set a shuffled color
                 const randomColorIndex = Math.floor(Math.random() * shuffledColors.length);
                 setBackgroundColor(div, shuffledColors[randomColorIndex]);
             }
         });
     }
-    
+
     function addBlackDotToSpecificDivs(div) {
         // Create a black dot element
         const blackDot = document.createElement('div');
@@ -82,41 +81,38 @@ document.addEventListener('DOMContentLoaded', function () {
         // Append the black dot to the specified div
         div.appendChild(blackDot);
     }
-    
 
-
-    // randomize initial colors everytime the page is relaoded 
+    // randomize initial colors every time the page is reloaded
     window.addEventListener('load', randomizeInitialColors);
 
     for (let row = 0; row < 7; row++) {
         for (let col = 0; col < 5; col++) {
             var cell = document.createElement('div');
             cell.classList.add('grid-cell');
-            cell.id = 'cell-' + cellCount; // Assign diff. ID to each cell
+            cell.id = 'cell-' + cellCount; // Assign different ID to each cell
             gridContainer.appendChild(cell);
             cellCount++; // increment cell count
         }
     }
 
-    //set the background color of a div
+    // set the background color of a div
     function setBackgroundColor(div, color) {
         div.style.backgroundColor = color;
     }
 
-gridContainer.addEventListener('click', (event) => {
-    const targetDiv = event.target; // Get the clicked element within the container
+    gridContainer.addEventListener('click', (event) => {
+        const targetDiv = event.target; // clicked element within the container
 
-    // if the target div is one of the specified fixed divs
-    if (targetDiv.classList.contains('grid-cell') && 
-        (targetDiv.id === 'cell-0' || targetDiv.id === 'cell-2' || targetDiv.id === 'cell-4' || targetDiv.id === 'cell-7'|| targetDiv.id === 'cell-12'||targetDiv.id === 'cell-15'|| targetDiv.id === 'cell-16'||targetDiv.id === 'cell-17'|| targetDiv.id === 'cell-18'|| targetDiv.id === 'cell-19'|| targetDiv.id === 'cell-22'|| targetDiv.id === 'cell-27'|| targetDiv.id === 'cell-30'|| targetDiv.id === 'cell-32'|| targetDiv.id === 'cell-34')) {
-        return; // Do nothing for the specified cells
-    }
+        // If  target div is specified fixed divs
+        if (targetDiv.classList.contains('grid-cell') &&
+            (targetDiv.id === 'cell-0' || targetDiv.id === 'cell-2' || targetDiv.id === 'cell-4' || targetDiv.id === 'cell-7' || targetDiv.id === 'cell-12' || targetDiv.id === 'cell-15' || targetDiv.id === 'cell-16' || targetDiv.id === 'cell-17' || targetDiv.id === 'cell-18' || targetDiv.id === 'cell-19' || targetDiv.id === 'cell-22' || targetDiv.id === 'cell-27' || targetDiv.id === 'cell-30' || targetDiv.id === 'cell-32' || targetDiv.id === 'cell-34')) {
+            return; // Do nothing for the cells
+        }
 
-    if (targetDiv.classList.contains('grid-cell')) {
-        handleDivSelection(targetDiv);
-    }
-});
-
+        if (targetDiv.classList.contains('grid-cell')) {
+            handleDivSelection(targetDiv);
+        }
+    });
 
     document.addEventListener('keypress', (event) => {
         if (event.key === ' ') {
@@ -125,13 +121,12 @@ gridContainer.addEventListener('click', (event) => {
         }
     });
 
-    //  variables
+    // variables
     let selectedDiv = null; // store the selected div
     let spaceBarPressed = false; // Flag to track space bar press
     let colorIndex = 0; // index for cycling colors
-    let audioPlayed = false; // to check if audio has been played
 
-    //  predetermined colors for each box
+    //  preset colors for each box
             const predeterminedColors = {
                 'cell-0': 'RGB(189, 64, 76 )',
                 'cell-1': 'RGB(191, 78, 71)',
@@ -176,59 +171,55 @@ gridContainer.addEventListener('click', (event) => {
                 'cell-34': 'RGB(173, 66, 130)',
 
             };
- 
+
             var audio = new Audio('completeSound.mp3');
-            // audio.preload = 'auto'; // Preload the audio
-            // audio.load(); // Load the audio
-
-    // cycle through colors
-    function cycleColors() {
-        if (selectedDiv && spaceBarPressed) {
-            // Check if the selected cell is one of the specified cells
-            if (selectedDiv.id === 'cell-0' || selectedDiv.id === 'cell-2' || selectedDiv.id === 'cell-4' || selectedDiv.id === 'cell-7' || selectedDiv.id === 'cell-12' || selectedDiv.id === 'cell-15' || selectedDiv.id === 'cell-16' || selectedDiv.id === 'cell-17' || selectedDiv.id === 'cell-18' || selectedDiv.id === 'cell-19' || selectedDiv.id === 'cell-22' || selectedDiv.id === 'cell-27' || selectedDiv.id === 'cell-30' || selectedDiv.id === 'cell-32' || selectedDiv.id === 'cell-34') {
-                return; // don't cycle these cells
-            }
-    
-            setBackgroundColor(selectedDiv, shuffledColors[colorIndex]);
-            colorIndex = (colorIndex + 1) % shuffledColors.length;
-    
-            if (selectedDiv.style.backgroundColor === predeterminedColors[selectedDiv]) {
-                // Play audio 
-                if (!audioPlayed) {
-                    audio.play();
-                    audioPlayed = true;
-                }
-                // Stop cycling colors
+            audio.preload = 'auto'; 
+            audio.load(); 
+        
+            audio.addEventListener('ended', () => {
                 spaceBarPressed = false;
+            });
+        
+            // cycle through colors
+            function cycleColors() {
+                if (selectedDiv && spaceBarPressed) {
+                    // Check if the selected cell is one of the specified cells
+                    if (selectedDiv.id === 'cell-0' || selectedDiv.id === 'cell-2' || selectedDiv.id === 'cell-4' || selectedDiv.id === 'cell-7' || selectedDiv.id === 'cell-12' || selectedDiv.id === 'cell-15' || selectedDiv.id === 'cell-16' || selectedDiv.id === 'cell-17' || selectedDiv.id === 'cell-18' || selectedDiv.id === 'cell-19' || selectedDiv.id === 'cell-22' || selectedDiv.id === 'cell-27' || selectedDiv.id === 'cell-30' || selectedDiv.id === 'cell-32' || selectedDiv.id === 'cell-34') {
+                        return; // don't cycle these cells
+                    }
+        
+                    setBackgroundColor(selectedDiv, shuffledColors[colorIndex]);
+                    colorIndex = (colorIndex + 1) % shuffledColors.length;
+        
+                    if (selectedDiv.style.backgroundColor === predeterminedColors[selectedDiv.id]) {
+                        // Play audio
+                        audio.play().then(() => {
+                        }).catch((error) => {
+                            console.error('Error playing audio:', error);
+                        });
+                    }
+                }
             }
-        }
-    }
-
-
-    function handleDivSelection(div) {
-        if (selectedDiv !== div) {
-            if (selectedDiv) {
-                // Remove the previous border class
-                selectedDiv.classList.remove('selected-border');
+        
+            function handleDivSelection(div) {
+                if (selectedDiv !== div) {
+                    if (selectedDiv) {
+                        // Remove the border 
+                        selectedDiv.classList.remove('selected-border');
+                    }
+                    selectedDiv = div;
+                    selectedDiv.classList.add('selected-border');
+                    colorIndex = 0;
+                }
             }
-            selectedDiv = div;
-            // Add the border class to the selected div
-            selectedDiv.classList.add('selected-border');
-            colorIndex = 0;
-        }
-    }
-    
-    
-
-   //  background colors for all cells
-   var cells = document.querySelectorAll('.grid-cell');
-   cells.forEach(function (cell) {
-       const cellId = cell.id;
-       setBackgroundColor(cell, predeterminedColors[cellId]);
-   });
-
-});
-
+        
+            // background colors for all cells
+            var cells = document.querySelectorAll('.grid-cell');
+            cells.forEach(function (cell) {
+                const cellId = cell.id;
+                setBackgroundColor(cell, predeterminedColors[cellId]);
+            });
+        });
 
 
  
